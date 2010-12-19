b2cb8387d603a086ce42115974201900b7ac9219
Ignas Rudaitis <ignas.rudaitis@gmail.com>
2010-12-19 13:04:55 +0200

    Fix for namespaced lambdas

--- includes/Splices.php
+++ includes/Splices.php
@@ -9,13 +9,14 @@
 namespace Patchwork\Splices;
 
 const CALL_HANDLING_SPLICE = '
-    $pwClass = __CLASS__ ? \get_called_class() : null;
+    $pwClosureName = __NAMESPACE__ ? __NAMESPACE__ . "\\{closure}" : "{closure}"; 
+    $pwClass = (__CLASS__ && __FUNCTION__ !== $pwClosureName) ? \get_called_class() : null;
     if (!empty($GLOBALS[\Patchwork\Patches\CALLBACKS][$pwClass][__FUNCTION__])) {
         if (\Patchwork\Patches\handle($pwClass, __FUNCTION__, \debug_backtrace(), $pwResult)) {
             return $pwResult;
         }
     }
-    unset($pwClass, $pwResult);
+    unset($pwClass, $pwResult, $pwClosureName);
 ';
 
 const EVAL_REPLACEMENT_SPLICE = '\Patchwork\Preprocessor\preprocessAndEval';

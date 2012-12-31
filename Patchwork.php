13c1945bafc56c8eb7d9a372a7290d7a32471a55
Ignas Rudaitis <ignas.rudaitis@gmail.com>
2012-12-31 12:23:06 +0200

    Removed opcode cache check, added version check

--- Patchwork.php
+++ Patchwork.php
@@ -8,12 +8,15 @@
  */
 namespace Patchwork;
 
+if (version_compare(PHP_VERSION, "5.3", "<")) {
+    trigger_error("Patchwork requires PHP version 5.3.0 or higher", E_USER_ERROR);
+}
+
 require_once __DIR__ . "/lib/Exceptions.php";
 require_once __DIR__ . "/lib/Interceptor.php";
 require_once __DIR__ . "/lib/Preprocessor.php";
 require_once __DIR__ . "/lib/Utils.php";
 require_once __DIR__ . "/lib/Stack.php";
-require_once __DIR__ . "/lib/CacheCheck.php";
 
 function replace($function, $replacement)
 {
@@ -39,8 +42,6 @@ function undoAll()
 {
     Interceptor\unpatchAll();
 }
-
-CacheCheck\run();
 
 Preprocessor\Stream::wrap();
 

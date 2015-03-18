b09c13594f96a43eb94fb833782a9582f658df93
Ignas Rudaitis <ignas.rudaitis@gmail.com>
2015-03-18 21:32:35 +0200

    Year bumped to 2015

--- src/Exceptions.php
+++ src/Exceptions.php
@@ -2,7 +2,7 @@
 
 /**
  * @author     Ignas Rudaitis <ignas.rudaitis@gmail.com>
- * @copyright  2010-2013 Ignas Rudaitis
+ * @copyright  2010-2015 Ignas Rudaitis
  * @license    http://www.opensource.org/licenses/mit-license.html
  * @link       http://antecedent.github.com/patchwork
  */
--- src/Preprocessor/Callbacks/Interceptor.php
+++ src/Preprocessor/Callbacks/Interceptor.php
@@ -2,7 +2,7 @@
 
 /**
  * @author     Ignas Rudaitis <ignas.rudaitis@gmail.com>
- * @copyright  2010-2014 Ignas Rudaitis
+ * @copyright  2010-2015 Ignas Rudaitis
  * @license    http://www.opensource.org/licenses/mit-license.html
  * @link       http://antecedent.github.com/patchwork
  */

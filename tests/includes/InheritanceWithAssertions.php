981b9228a2a281cae5694ec28e9fcdec0745be9d
Ignas Rudaitis <ignas.rudaitis@gmail.com>
2015-10-09 20:05:35 +0300

    Further attempts to fix HHVM issues (7)

---
+++ tests/includes/InheritanceWithAssertions.php
@@ -0,0 +1,48 @@
+<?php
+
+class FooObject
+{
+    function getFoo()
+    {
+        return $this->bah();
+    }
+
+    private function bah()
+    {
+        return "foo";
+    }
+}
+
+class BarObject extends FooObject
+{
+    function getBar()
+    {
+        return "bar";
+    }
+}
+
+class BazObject extends BarObject
+{
+    function getBaz()
+    {
+        return "baz";
+    }
+
+    function getBar()
+    {
+        return "bar (overridden)";
+    }
+}
+
+$foo = new FooObject;
+$bar = new BarObject;
+$baz = new BazObject;
+
+assert($foo->getFoo() === "foo");
+
+assert($bar->getFoo() === "foo (patched)");
+assert($bar->getBar() === "bar (patched)");
+
+assert($baz->getFoo() === "foo (patched)");
+assert($baz->getBar() === "bar (overridden)");
+assert($baz->getBaz() === "baz");
---
+++ tests/inheritance-delayed-redefinition.phpt
@@ -0,0 +1,27 @@
+--TEST--
+Inheriting method patches + delayed redefinition (suspected HHVM issue)
+
+--FILE--
+<?php
+
+assert_options(ASSERT_ACTIVE, 1);
+assert_options(ASSERT_WARNING, 1);
+error_reporting(E_ALL | E_STRICT);
+
+require __DIR__ . "/../Patchwork.php";
+
+Patchwork\replace("BarObject::getFoo", function() {
+    return "foo (patched)";
+});
+
+Patchwork\replace("BarObject::getBar", function() {
+    return "bar (patched)";
+});
+
+require __DIR__ . "/includes/InheritanceWithAssertions.php";
+
+?>
+===DONE===
+
+--EXPECT--
+===DONE===

2baeeaf5ce63b2f599e8333cab5c3007d5f34c10
Ignas Rudaitis <ignas.rudaitis@gmail.com>
2017-05-03 21:57:07 +0300

    #55

--- src/CodeManipulation/Actions/ConflictPrevention.php
+++ src/CodeManipulation/Actions/ConflictPrevention.php
@@ -19,12 +19,15 @@ function preventImportingOtherCopiesOfPatchwork()
 {
     return function(Source $s) {
         $namespaceKeyword = $s->next(T_NAMESPACE, -1);
-        if ($namespaceKeyword === INF) {
+        if ($namespaceKeyword === INF || $namespaceKeyword < 2) {
             return;
         }
         if ($s->read($namespaceKeyword, 4) == 'namespace Patchwork;') {
-            # Clear the file completely (in memory)
-            $s->splice('', 0, count($s->tokens));
+            $pattern = '/@copyright\s+2010-\d+ Ignas Rudaitis/';
+            if (preg_match($pattern, $s->read($namespaceKeyword - 2))) {
+                # Clear the file completely (in memory)
+                $s->splice('', 0, count($s->tokens));
+            }
         }
     };
 }

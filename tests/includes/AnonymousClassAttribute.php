c8a47ac839f915d234827937c1f29bafae12b71d
Romain Canon <romain.hydrocanon@gmail.com>
2024-02-01 13:08:07 +0100

    Add more cases in anonymous class attribute test

--- tests/includes/AnonymousClassAttribute.php
+++ tests/includes/AnonymousClassAttribute.php
@@ -1,6 +1,18 @@
 <?php
 
 #[\Attribute]
-class SomeAttribute {}
+class SomeAttribute {
+    public function __construct($value) {}
+}
 
-new #[SomeAttribute] class {};
+// Simple attribute
+new #[SomeAttribute('foo')] class {};
+
+// Several attributes
+new #[SomeAttribute('foo')] #[SomeAttribute('bar')] class {};
+
+// Attribute with argument on several lines
+new #[SomeAttribute([
+    'foo',
+    'bar',
+])] class {};
